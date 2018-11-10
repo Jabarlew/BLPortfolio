3b498ec6b7c404ed2d9e890002af0a0663e87121
Matthew Palmer <matthew@compass.digital.io>
2018-11-10 13:24:58 -0500

    add pug and fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,13 @@
 'use strict';
 
 const path = require('path');
-const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
 
-const dirs = {
-  js: path.resolve('src/js'),
-  dist: path.resolve('dist'),
-};
-
-const base = {
-  context: dirs.js,
+module.exports = {
+  context: path.resolve('src/js'),
   entry: './index.js',
   output: {
-    path: dirs.dist,
+    path: path.resolve('dist'),
     filename: 'assets/[name].js',
     publicPath: '/',
   },
 };
-
-const environments = {
-  development: {
-    mode: 'development',
-    plugins: [new webpack.HotModuleReplacementPlugin()],
-    devServer: {
-      host: '0.0.0.0',
-      publicPath: '/',
-      inline: true,
-      hot: true,
-      historyApiFallback: true,
-      quiet: true,
-      stats: 'minimal',
-      clientLogLevel: 'none',
-      disableHostCheck: true,
-    },
-  },
-
-  production: { mode: 'production' },
-};
-
-module.exports = env => webpackMerge(base, environments[env]);
